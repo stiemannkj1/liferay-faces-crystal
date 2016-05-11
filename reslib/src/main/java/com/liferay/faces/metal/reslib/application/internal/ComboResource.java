84d163c5e279b28c65ac0dd621be4920228a4c22
Kyle Stiemann <stiemannkj1@gmail.com>
2016-05-11 15:21:08 -0400

    Reslib TODO

---
+++ reslib/src/main/java/com/liferay/faces/metal/reslib/application/internal/ComboResource.java
@@ -0,0 +1,144 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.metal.reslib.application.internal;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import javax.faces.application.Resource;
+import javax.faces.application.ResourceHandler;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+import com.liferay.faces.util.ContentTypes;
+import com.liferay.faces.util.application.ResourceUtil;
+import com.liferay.faces.util.logging.Logger;
+import com.liferay.faces.util.logging.LoggerFactory;
+
+
+/**
+ * @author  Neil Griffin
+ */
+public class ComboResource extends Resource {
+
+	// Public Constants
+	public static final String RESOURCE_NAME = "combo";
+	public static final String DUMMY_RESOURCE_NAME = "combo-resource.txt";
+
+	// Logger
+	private static final Logger logger = LoggerFactory.getLogger(ComboResource.class);
+
+	// Private Constants
+	private static final String RESOURCE_PATH_BASE = "META-INF/resources/liferay-faces-metal-reslib/";
+
+	// Private Data Members
+	private List<String> modulePaths;
+	private String requestPath;
+
+	public ComboResource(List<String> modulePaths) {
+
+		this.modulePaths = modulePaths;
+		setLibraryName(ResLibResourceHandler.LIBRARY_NAME);
+		setResourceName(RESOURCE_NAME);
+	}
+
+	@Override
+	public boolean userAgentNeedsUpdate(FacesContext facesContext) {
+		return true;
+	}
+
+	@Override
+	public String getContentType() {
+
+		String contentType = ContentTypes.TEXT_PLAIN;
+
+		if ((modulePaths != null) && (modulePaths.size() > 0)) {
+			String firstModulePath = modulePaths.get(0);
+
+			if (firstModulePath.endsWith(".css")) {
+				contentType = ContentTypes.TEXT_CSS;
+			}
+			else if (firstModulePath.endsWith(".js")) {
+				contentType = ContentTypes.TEXT_JAVASCRIPT;
+			}
+		}
+
+		return contentType;
+	}
+
+	@Override
+	public InputStream getInputStream() throws IOException {
+
+		StringBuilder comboResourceText = new StringBuilder();
+		String contentType = getContentType();
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ResourceHandler resourceHandlerChain = facesContext.getApplication().getResourceHandler();
+		ExternalContext externalContext = facesContext.getExternalContext();
+
+		for (String modulePath : modulePaths) {
+
+			String resourcePath = RESOURCE_PATH_BASE + modulePath;
+			URL resourceURL = ComboResource.class.getClassLoader().getResource(resourcePath);
+
+			logger.debug("resourcePath=[{0}] resourceURL=[{1}]", resourcePath, resourceURL);
+
+			InputStream inputStream = resourceURL.openStream();
+			String resourceText = ResourceUtil.toString(inputStream, "UTF-8", 1024);
+
+			if (ContentTypes.TEXT_CSS.equals(contentType)) {
+				resourceText = ExpressionUtil.filterExpressions(resourceText, resourceHandlerChain, externalContext);
+			}
+
+			comboResourceText.append(resourceText);
+		}
+
+		return ResourceUtil.toInputStream(comboResourceText.toString(), "UTF-8");
+	}
+
+	@Override
+	public String getRequestPath() {
+
+		if (requestPath == null) {
+
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			ResourceHandler resourceHandlerChain = facesContext.getApplication().getResourceHandler();
+			Resource dummyResource = resourceHandlerChain.createResource(DUMMY_RESOURCE_NAME, getLibraryName());
+			String dummyResourceRequestPath = dummyResource.getRequestPath();
+			requestPath = dummyResourceRequestPath.replace(DUMMY_RESOURCE_NAME, RESOURCE_NAME);
+		}
+
+		return requestPath;
+	}
+
+	@Override
+	public Map<String, String> getResponseHeaders() {
+		return Collections.EMPTY_MAP;
+	}
+
+	@Override
+	public URL getURL() {
+		return null;
+	}
+}
---
+++ reslib/src/main/java/com/liferay/faces/metal/reslib/application/internal/ResLibResourceHandler.java
@@ -0,0 +1,320 @@
+/**
+ * Copyright (c) 2000-2016 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+package com.liferay.faces.metal.reslib.application.internal;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.faces.application.Resource;
+import javax.faces.application.ResourceHandler;
+import javax.faces.application.ResourceHandlerWrapper;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.liferay.faces.metal.reslib.config.ResLibConfigParam;
+import com.liferay.faces.util.HttpHeaders;
+import com.liferay.faces.util.config.ApplicationConfig;
+import com.liferay.faces.util.config.ConfiguredServletMapping;
+import com.liferay.faces.util.config.FacesConfig;
+import com.liferay.faces.util.product.ProductConstants;
+import com.liferay.faces.util.product.ProductMap;
+
+
+/**
+ * This is a resource handler that is only necessary in a non-Liferay (webapp) environment. The purpose of this class is
+ * to provide Metal/YUI/Bootstrap resources.
+ *
+ * @author  Neil Griffin
+ */
+public class ResLibResourceHandler extends ResourceHandlerWrapper {
+
+	// Public Constants
+	public static final String LIBRARY_NAME = "liferay-faces-metal-reslib";
+
+	// Private Constants
+	private static final String LIFERAY_JS = "liferay.js";
+	private static final boolean LIFERAY_PORTAL_DETECTED = ProductMap.getInstance().get(ProductConstants.LIFERAY_PORTAL)
+		.isDetected();
+	private static final String MINIFIER_TYPE = "minifierType";
+	private static final Set<String> PROTECTED_PARAMTERS;
+
+	static {
+		PROTECTED_PARAMTERS = new HashSet<String>();
+		PROTECTED_PARAMTERS.add("b");
+		PROTECTED_PARAMTERS.add("browserId");
+		PROTECTED_PARAMTERS.add("languageId");
+		PROTECTED_PARAMTERS.add("ln");
+		PROTECTED_PARAMTERS.add(MINIFIER_TYPE);
+		PROTECTED_PARAMTERS.add("t");
+	}
+
+	// Private Members
+	private ResourceHandler wrappedResourceHandler;
+
+	public ResLibResourceHandler(ResourceHandler wrappedResourceHandler) {
+		this.wrappedResourceHandler = wrappedResourceHandler;
+	}
+
+	@Override
+	public Resource createResource(String resourceName) {
+
+		Resource resource;
+
+		if (!LIFERAY_PORTAL_DETECTED) {
+
+			if (ComboResource.RESOURCE_NAME.equals(resourceName)) {
+
+				FacesContext facesContext = FacesContext.getCurrentInstance();
+				ExternalContext externalContext = facesContext.getExternalContext();
+				List<String> modulePaths = getModulePaths(externalContext);
+				resource = new ComboResource(modulePaths);
+			}
+			else if (ScriptResource.RESOURCE_NAME.equals(resourceName)) {
+
+				FacesContext facesContext = FacesContext.getCurrentInstance();
+				ExternalContext externalContext = facesContext.getExternalContext();
+				List<String> modulePaths = getModulePaths(externalContext);
+				String scriptModulePath = "";
+
+				if (modulePaths.size() > 0) {
+					scriptModulePath = modulePaths.get(0);
+				}
+
+				resource = new ScriptResource(scriptModulePath);
+			}
+			else {
+
+				resource = super.createResource(resourceName);
+
+				if (LIFERAY_JS.equals(resourceName)) {
+					resource = new FilteredResourceExpressionImpl(resource);
+				}
+			}
+		}
+		else {
+			resource = super.createResource(resourceName);
+		}
+
+		return resource;
+	}
+
+	@Override
+	public Resource createResource(String resourceName, String libraryName) {
+
+		Resource resource;
+
+		if (!LIFERAY_PORTAL_DETECTED && LIBRARY_NAME.equals(libraryName)) {
+
+			if (ComboResource.RESOURCE_NAME.equals(resourceName) || ScriptResource.RESOURCE_NAME.equals(resourceName)) {
+				resource = createResource(resourceName);
+			}
+			else {
+
+				resource = super.createResource(resourceName, libraryName);
+
+				if (LIFERAY_JS.equals(resourceName)) {
+					resource = new FilteredResourceExpressionImpl(resource);
+				}
+			}
+		}
+		else {
+			resource = super.createResource(resourceName, libraryName);
+		}
+
+		return resource;
+	}
+
+	@Override
+	public Resource createResource(String resourceName, String libraryName, String contentType) {
+
+		Resource resource;
+
+		if (!LIFERAY_PORTAL_DETECTED && LIBRARY_NAME.equals(libraryName)) {
+
+			if (ComboResource.RESOURCE_NAME.equals(resourceName) || ScriptResource.RESOURCE_NAME.equals(resourceName)) {
+				resource = createResource(resourceName);
+			}
+			else {
+
+				resource = super.createResource(resourceName, libraryName, contentType);
+
+				if (LIFERAY_JS.equals(resourceName)) {
+					resource = new FilteredResourceExpressionImpl(resource);
+				}
+			}
+		}
+		else {
+			resource = super.createResource(resourceName, libraryName, contentType);
+		}
+
+		return resource;
+	}
+
+	@Override
+	public void handleResourceRequest(FacesContext facesContext) throws IOException {
+
+		if (LIFERAY_PORTAL_DETECTED) {
+			super.handleResourceRequest(facesContext);
+		}
+		else {
+
+			ExternalContext externalContext = facesContext.getExternalContext();
+			Map<String, String> requestParameterMap = externalContext.getRequestParameterMap();
+			String libraryName = requestParameterMap.get("ln");
+			String resourceName = getResourceName(externalContext);
+
+			// If the resource that is to be rendered is a combo or script module resource and the module paths
+			// extensions are invalid, then the resource cannot be found.
+			if (LIBRARY_NAME.equals(libraryName) &&
+					(ComboResource.RESOURCE_NAME.equals(resourceName) ||
+						ScriptResource.RESOURCE_NAME.equals(resourceName)) &&
+					!areModulePathsExtensionsValid(externalContext)) {
+
+				externalContext.setResponseHeader(HttpHeaders.CACHE_CONTROL, HttpHeaders.CACHE_CONTROL_NO_CACHE_VALUE);
+				externalContext.setResponseStatus(HttpServletResponse.SC_NOT_FOUND);
+			}
+
+			// Otherwise, pass responsibility for handling the resource to the resource-handler delegation chain.
+			else {
+				super.handleResourceRequest(facesContext);
+			}
+		}
+	}
+
+	@Override
+	public boolean libraryExists(String libraryName) {
+
+		if (!LIFERAY_PORTAL_DETECTED && LIBRARY_NAME.equals(libraryName)) {
+			return true;
+		}
+		else {
+			return super.libraryExists(libraryName);
+		}
+	}
+
+	protected boolean areModulePathsExtensionsValid(ExternalContext externalContext) {
+
+		List<String> modulePaths = getModulePaths(externalContext);
+		boolean modulePathsExtensionsValid = false;
+
+		if (modulePaths.size() > 0) {
+
+			String[] comboAllowedFileExtensions = ResLibConfigParam.ComboAllowedFileExtensions.getStringValue(
+					externalContext).split(",");
+			modulePathsExtensionsValid = true;
+
+			for (String modulePath : modulePaths) {
+
+				boolean validFileExtension = false;
+
+				for (String comboAllowedFileExtension : comboAllowedFileExtensions) {
+
+					if (modulePath.endsWith(comboAllowedFileExtension)) {
+
+						validFileExtension = true;
+
+						break;
+					}
+				}
+
+				if (!validFileExtension) {
+
+					modulePathsExtensionsValid = false;
+
+					break;
+				}
+			}
+		}
+
+		return modulePathsExtensionsValid;
+	}
+
+	protected List<String> getModulePaths(ExternalContext externalContext) {
+
+		List<String> modulePaths = new ArrayList<String>();
+		Iterator<String> requestParameterNames = externalContext.getRequestParameterNames();
+
+		while (requestParameterNames.hasNext()) {
+			String paramterName = requestParameterNames.next();
+
+			if (!PROTECTED_PARAMTERS.contains(paramterName)) {
+				modulePaths.add(paramterName);
+			}
+		}
+
+		return modulePaths;
+	}
+
+	protected String getResourceName(ExternalContext externalContext) {
+
+		// Attempt to get the resource name from the "javax.faces.resource" request parameter. If it exists, then
+		// this is probably a non-Liferay portlet environment like Pluto.
+		String resourceName = externalContext.getRequestParameterMap().get("javax.faces.resource");
+
+		if (resourceName == null) {
+
+			// If the specified request was extension-mapped (suffix-mapped), then determine the resource name based
+			// on the configured mappings to the Faces Servlet.
+			Object request = externalContext.getRequest();
+
+			if (request instanceof HttpServletRequest) {
+				HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+				String servletPath = httpServletRequest.getServletPath();
+
+				if ((servletPath != null) && servletPath.startsWith(RESOURCE_IDENTIFIER)) {
+
+					Map<String, Object> applicationMap = externalContext.getApplicationMap();
+					String appConfigAttrName = ApplicationConfig.class.getName();
+					ApplicationConfig applicationConfig = (ApplicationConfig) applicationMap.get(appConfigAttrName);
+					FacesConfig facesConfig = applicationConfig.getFacesConfig();
+					List<ConfiguredServletMapping> configuredFacesServletMappings =
+						facesConfig.getConfiguredFacesServletMappings();
+
+					resourceName = servletPath.substring(RESOURCE_IDENTIFIER.length() + 1);
+
+					for (ConfiguredServletMapping configuredFacesServletMapping : configuredFacesServletMappings) {
+
+						String configuredExtension = configuredFacesServletMapping.getExtension();
+
+						if (servletPath.endsWith(configuredExtension)) {
+							resourceName = resourceName.substring(0,
+									resourceName.length() - configuredExtension.length());
+
+							break;
+						}
+					}
+				}
+
+				// Otherwise, it must be path-mapped.
+				else {
+					resourceName = httpServletRequest.getPathInfo();
+				}
+			}
+		}
+
+		return resourceName;
+	}
+
+	@Override
+	public ResourceHandler getWrapped() {
+		return wrappedResourceHandler;
+	}
+}
